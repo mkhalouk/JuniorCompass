1556953818ab480521c241d302ba0ff8d60dd152
mkhalouk <musta.khalouk@gmail.com>
2023-04-19 01:27:20 +0200

    config: initialized and configured empty project spring + react

---
+++ backend/src/main/java/com/juniorcompass/app/JuniorCompassApplication.java
@@ -0,0 +1,13 @@
+package com.juniorcompass.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JuniorCompassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JuniorCompassApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/juniorcompass/app/JuniorCompassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juniorcompass.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JuniorCompassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
